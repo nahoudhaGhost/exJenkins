1b6027dbca1a92d91c2abefa5344d65deed227a1
thomasreidon <thomas.reidon@gmail.com>
2019-05-03 09:55:06 +0200

    Codage Calculatrice

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("premier terme : ");
+        int x = sc.nextInt();
+        System.out.println("deuxieme terme : ");
+        int y = sc.nextInt();
+        Calculatrice c = new Calculatrice();
+        int somme = c.addition(x, y);
+        int produit = c.multiplication(x, y);
+        int retenu = c.soustraction(x, y);
+        int quotien = c.division(x, y);
+        int reste = c.modulo(x, y);
+        System.out.println(somme);
+        System.out.println(quotien);
+        System.out.println(reste);
+        System.out.println(retenu);
+        System.out.println(produit);
+        sc.close();
     }
 }
